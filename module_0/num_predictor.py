a589cffbdcdd08f1f5e35153f8dff41a75f9c250
demonolock <Rektor12gb>
2020-05-02 23:29:34 +0300

    add num predictor

---
+++ module_0/num_predictor.py
@@ -0,0 +1,68 @@
+import numpy as np
+MAX = 100
+
+
+def game_core_v1(number):
+    '''Просто угадываем на random ни как не используя информацию о больше или меньше.
+       Функция Принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1, MAX+1)  # предполагаемое число
+        if number == predict:
+            break
+    return count  # выход из цикла, если угадали
+
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его
+       в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    predict = np.random.randint(1, MAX)
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    return count  # выход из цикла, если угадали
+
+
+def game_core_v3(number):
+    '''Сначала устанавливаем любое random число, а потом находим с помощью алгоритма
+    двоичного поисканужное число.
+    Функция принимает загаданное число и возвращает число попыток'''
+
+    count = 0
+    mid = np.random.randint(1, MAX)
+    low = 0
+    high = MAX
+
+    while mid != number and low <= high:
+        count += 1
+        if number > mid:
+            low = mid + 1
+        else:
+            high = mid - 1
+        mid = (low + high) // 2
+
+    return count
+
+
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, MAX+1, size=1000)
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return score
+
+
+# запускаем все версии и сравниваем
+score_game(game_core_v1)
+score_game(game_core_v2)
+score_game(game_core_v3)
